3d9c9242cf2dd1a59018228cac82a862eb78ebb5
khs2617 <khs980714@gmail.com>
2025-03-02 18:11:40 +0900

    utils.py 를 추가(reset_seed) / models.py에 간단한 주석 추가 및 시드 고정 추가

--- service/models.py
+++ service/models.py
@@ -8,11 +8,15 @@ from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
 import joblib
 
+from utils import reset_seeds
+
 class ClassificationModels:
   def __init__(self, model:str='random_forest', **kwargs):
     self.model = model
     self.model = self._init_model(**kwargs)
   
+  # 모델 초기 설정
+  @reset_seeds
   def _init_model(self, **kwargs):
     if self.model == 'random_forest':
       return RandomForestClassifier(**kwargs)
@@ -25,24 +29,32 @@ class ClassificationModels:
     else:
       raise ValueError("model : 'random_forest', 'xgboost', 'lightgbm', 'catboost")
   
+  # 학습하기 위한 함수
+  @reset_seeds
   def train(self, feature, target):
       self.model.fit(feature, target)
   
+  # 예측하기 위한 함수
   def predict(self, X):
     return self.model.predict(X)
   
+  # 예측 확률값하기 위한 함수
   def predict_proba(self, X):
     return self.model.predict_proba(X)
 
+  # 예측 확률값을 threshold로 조정하기 위한 함수
   def threshold_pred(self, X, threshold = 0.5):
     return self.model.predict_proba(X)[:, 1] > threshold
   
+  # 모델을 불러오는 함수 (아마 필요 없을거같습니다)
   def get_model(self):
     return self.model
   
+  # 모델 저장학기 위한 함수
   def save_model(self, root:Path = Path('/models'), ):
     joblib.dump(self.model, root / self.model)
 
+  # 모델 불러오기 위한 함수
   def load_model(self, root:Path = Path('/models')):
     self.model = joblib.load(root / self.model)
 
